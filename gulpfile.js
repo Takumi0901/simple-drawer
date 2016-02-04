4d9f0587c2ec477b67cff00d011b1fba2eaf20f0
木村 匠 <kimura_taku@camobile.com>
2016-02-04 10:58:40 +0900

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -9,35 +9,35 @@ var gulp = require('gulp'),
 
 // sass　コンパイル
 gulp.task('sass', function() {
-    return sass('sass/', { style: 'expanded' })
+    return sass('src/sass/', { style: 'expanded' })
         .pipe(plumber())
         .pipe(header('@charset "utf-8";\n'))
         .pipe(csscomb())
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest('dist/css/'));
 });
 
 
 // cssの圧縮
 gulp.task('css:min', function() {
-     return gulp.src('css/**.css')
+     return gulp.src('dist/css/simple-drawer.css')
         .pipe(plumber())
     	.pipe(cssmin())
         .pipe(rename({
           extname: '.min.css'
         }))
-        .pipe(gulp.dest('css/dist/'));
+        .pipe(gulp.dest('dist/css/'));
 });
 
 
 // jsファイルを圧縮して.minを付与
 gulp.task("js:min", function() {
-    return gulp.src('js/*.js')
+    return gulp.src('dist/js/jquery.simpledrawer.js')
         .pipe(plumber())
         .pipe(uglify())
         .pipe(rename({
           extname: '.min.js'
         }))
-        .pipe(gulp.dest("js/dist/"));
+        .pipe(gulp.dest("dist/js/"));
 });
 
 
@@ -45,11 +45,11 @@ gulp.task("js:min", function() {
  * watch
  */
 gulp.task('watch', function(){
-    gulp.watch('sass/**/**/*', function(event) {
+    gulp.watch('src/sass/**/**/*', function(event) {
         gulp.run('sass');
         gulp.run('css:min');
     });
-    gulp.watch('js/*.js', function(event) {
+    gulp.watch('dist/js/jquery.simpledrawer.js', function(event) {
         gulp.run('js:min');
     });
 });
