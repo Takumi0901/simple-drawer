18316d14ec649f12aa86f7e40f24ff642620a0e3
t-kimura <juventus0901@gmail.com>
2016-04-01 17:45:43 +0900

    Android対応

--- dist/js/jquery.simpledrawer.js
+++ dist/js/jquery.simpledrawer.js
@@ -10,36 +10,36 @@
 		/**
 		 * [defaults デフォルト設定]
 		 */
-	    this.defaults = {
-	        btnside 		: 'left',
-	        drawerside 		: 'left',
-	        fixed 			: true
-	    };
+			this.defaults = {
+					btnside 		: 'left',
+					drawerside 		: 'left',
+					fixed 			: true
+			};
 
 
-	    /**
-	     * [elements クラスセレクタ定義]
-	     */
-	    this.elements = {
-	    	btn 			: '.simple-drawer-btn',
-	    	bar 			: '.simple-drawer-fixed-bar',
-	    	drawer_inner 	: '.simple-drawer-inner',
-	    	overlay 		: '.simple-drawer-overlay',
-	    	wrapper 		: '.simple-drawer-wrapper'
-	    };
+			/**
+			 * [elements クラスセレクタ定義]
+			 */
+			this.elements = {
+				btn 			: '.simple-drawer-btn',
+				bar 			: '.simple-drawer-fixed-bar',
+				drawer_inner 	: '.simple-drawer-inner',
+				overlay 		: '.simple-drawer-overlay',
+				wrapper 		: '.simple-drawer-wrapper'
+			};
 
 
-	    /**
-	     * [setting this.defaultsをoptionで上書き]
-	     */
-	    this.setting = $.extend(this.defaults, option);
+			/**
+			 * [setting this.defaultsをoptionで上書き]
+			 */
+			this.setting = $.extend(this.defaults, option);
 
-	    this.initialize(el);
-	    this.handleEvents();
+			this.initialize(el);
+			this.handleEvents();
 
-	    if(this.setting.fixed == true){
-	    	this.fixedBarOnScroll();
-	    }
+			if(this.setting.fixed == true){
+				this.fixedBarOnScroll();
+			}
 		
 	}
 	$.extend($.simpleDrawer.prototype,{
@@ -49,55 +49,56 @@
 		 * [initialize 初期化]
 		 * @param  {[type]} el [drawer オブジェクト]
 		 */
-        initialize : function(el){
-        	this.$el = $(el);
-        	this.$btn = $(this.elements.btn);
-        	this.$fixed_bar = $(this.elements.bar);
-        	this.$window = $(window);
+				initialize : function(el){
+					this.$el = $(el);
+					this.$btn = $(this.elements.btn);
+					this.$fixed_bar = $(this.elements.bar);
+					this.$window = $(window);
 
-        	this.start_pos = 0;
+					this.start_pos = 0;
 
-        	this.elementBtnSide();
-        	this.elementDrawerSide();
-        	this.drawerInnerHeight();
-        	this.drawerPartsAppend();
-        },
+					this.elementBtnSide();
+					this.elementDrawerSide();
+					this.drawerInnerHeight();
+					this.drawerPartsAppend();
+				},
 
 
-        /**
-         * [handleEvents タッチイベント]
-         */
-        handleEvents: function(){
-        	var self = this;
-		    this.$btn.on('touchstart click', function(){
-		    	if(self.$el.hasClass('is-open')){
-		            self.drawerSlideClose();
-		            self.wrapperScrollOff();
-		        }else{
-		            self.drawerSlideOpen();
-		            self.wrapperScrollOn();
-		        }
-		        return false;
-		    });
+				/**
+				 * [handleEvents タッチイベント]
+				 */
+				handleEvents: function(){
+					var self = this;
+					var _touch = ('ontouchstart' in document) ? 'touchstart' : 'click';
+				this.$btn.on(_touch, function(){
+					if(self.$el.hasClass('is-open')){
+								self.drawerSlideClose();
+								self.wrapperScrollOff();
+						}else{
+								self.drawerSlideOpen();
+								self.wrapperScrollOn();
+						}
+						return false;
+				});
 
-		    this.$overlay.on('touchstart click', function(){
+				this.$overlay.on(_touch, function(){
 				self.drawerSlideClose();
 
 				return false;
-		    });		    	
-        },
+				});		    	
+				},
 
 
-        /**
-         * [drawerSlideOpen ドロワーを開く]
-         */
-        drawerSlideOpen: function(){
-	        this.$el.addClass('is-open');
-	        this.$btn.addClass('is-open');
-	        this.$overlay.addClass('is-fadein');
-	        this.$fixed_bar.addClass('is-open');
+				/**
+				 * [drawerSlideOpen ドロワーを開く]
+				 */
+				drawerSlideOpen: function(){
+					this.$el.addClass('is-open');
+					this.$btn.addClass('is-open');
+					this.$overlay.addClass('is-fadein');
+					this.$fixed_bar.addClass('is-open');
 
-		    return false;
+				return false;
 		},
 
 
@@ -105,17 +106,17 @@
 		 * [drawerSlideClose ドロワーを閉じる]
 		 */
 		drawerSlideClose: function(){
-		    var self = this;
+				var self = this;
 
-		    this.$el.removeClass('is-open');
-		    this.$btn.removeClass('is-open');
-		    this.$overlay.removeClass('is-fadein');
+				this.$el.removeClass('is-open');
+				this.$btn.removeClass('is-open');
+				this.$overlay.removeClass('is-fadein');
 
-		    setTimeout(function(){
-		        self.$fixed_bar.removeClass('is-open');
-		    },300);
+				setTimeout(function(){
+						self.$fixed_bar.removeClass('is-open');
+				},300);
 
-		    return false;
+				return false;
 		},
 
 
@@ -123,13 +124,13 @@
 		 * [drawerInnerHeight ドロワー内スクロール用の高さを算出]
 		 */
 		drawerInnerHeight: function(){
-		    var $target = this.$el.children(this.elements.drawer_inner);
-		    var $target_height = $target.height();
-		    var $window_height = $(window).height();
+				var $target = this.$el.children(this.elements.drawer_inner);
+				var $target_height = $target.height();
+				var $window_height = $(window).height();
 
-		    if($window_height < $target_height){
-		        $target.css('height', $window_height - 80);  
-		    }
+				if($window_height < $target_height){
+						$target.css('height', $window_height - 80);  
+				}
 		},
 		
 
@@ -138,12 +139,12 @@
 		 */
 		drawerPartsAppend: function(){
 			var overlaySlice = this.elements.overlay.substr(1);
-        	this.$overlayParts = '<div class="' + overlaySlice + ' c-drawer__overlay"></div>';
+					this.$overlayParts = '<div class="' + overlaySlice + ' c-drawer__overlay"></div>';
 
-		    if($(this.elements.overlay).length <= 0){
-		        $('body').append(this.$overlayParts);
-		    }
-		    this.$overlay = $(this.elements.overlay);
+				if($(this.elements.overlay).length <= 0){
+						$('body').append(this.$overlayParts);
+				}
+				this.$overlay = $(this.elements.overlay);
 		},
 
 
@@ -151,9 +152,9 @@
 		 * [wrapperScrollOff メインコンテンツのスクロールを禁止 SP]
 		 */
 		wrapperScrollOff: function(){
-		    $(this.setting.wrapper).on('touchmove.noScroll', function(e) {
-		        e.preventDefault();
-		    });
+				$(this.setting.wrapper).on('touchmove.noScroll', function(e) {
+						e.preventDefault();
+				});
 		},
 
 
@@ -161,7 +162,7 @@
 		 * [wrapperScrollOn メインコンテンツのスクロールを禁止解除 SP]
 		 */
 		wrapperScrollOn: function(){
-		    $(this.elements.wrapper).off('.noScroll');
+				$(this.elements.wrapper).off('.noScroll');
 		},
 
 
@@ -170,16 +171,16 @@
 		 */
 		elementBtnSide: function(){
 			if(this.setting.btnside == 'left'){
-		    	this.$btn.addClass('is-left');
+					this.$btn.addClass('is-left');
 
-		    	return false;
+					return false;
 
-		    }else if(this.setting.btnside == 'right'){
-		    	this.$btn.addClass('is-right');
+				}else if(this.setting.btnside == 'right'){
+					this.$btn.addClass('is-right');
 
-		    	return false;
+					return false;
 
-		    }
+				}
 		},
 
 
@@ -188,16 +189,16 @@
 		 */
 		elementDrawerSide: function(){
 			if(this.setting.drawerside == 'left'){
-		    	this.$el.addClass('is-left');
+					this.$el.addClass('is-left');
 
-		    	return false;
+					return false;
 
-		    }else if(this.setting.drawerside == 'right'){
-		    	this.$el.addClass('is-right');
+				}else if(this.setting.drawerside == 'right'){
+					this.$el.addClass('is-right');
 
-		    	return false;
+					return false;
 
-		    }
+				}
 		},
 
 
@@ -205,26 +206,26 @@
 		 * [fixedBarOnScroll fixed-barのスクロール判定]
 		 */
 		fixedBarOnScroll: function(){
-		    var self = this;
-		    var _touch = ('ontouchstart' in document) ? 'touchmove' : 'scroll';
+				var self = this;
+				var _touch = ('ontouchstart' in document) ? 'touchmove' : 'scroll';
 
-		    this.$window.on(_touch, function(){
-		        var $current_pos = $(window).scrollTop();
+				this.$window.on(_touch, function(){
+						var $current_pos = $(window).scrollTop();
 
-		         // drawerが開いていたら動作しない
-		        if(!self.$btn.hasClass('is-open')){
-		            if($current_pos > 0){
-		                if ($current_pos > self.start_pos) {
-		                    self.fixedBarSlideOut();
-		                }else{
-		                    self.fixedBarSlideIn();
-		                }
-		            }else{
-		                self.fixedBarSlideIn();
-		            }
-		            self.start_pos = $current_pos;   
-		        }
-		    });
+						 // drawerが開いていたら動作しない
+						if(!self.$btn.hasClass('is-open')){
+								if($current_pos > 0){
+										if ($current_pos > self.start_pos) {
+												self.fixedBarSlideOut();
+										}else{
+												self.fixedBarSlideIn();
+										}
+								}else{
+										self.fixedBarSlideIn();
+								}
+								self.start_pos = $current_pos;   
+						}
+				});
 		},
 
 
@@ -232,7 +233,7 @@
 		 * [fixedBarSlideIn fixed-bar スライドイン]
 		 */
 		fixedBarSlideIn: function(){
-		    this.$fixed_bar.removeClass('is-slideout');
+				this.$fixed_bar.removeClass('is-slideout');
 		},
 
 
@@ -241,9 +242,9 @@
 		 * [fixedBarSlideOut fixed-bar スライドアウト]
 		 */
 		fixedBarSlideOut: function(){
-		    this.$fixed_bar.addClass('is-slideout');
+				this.$fixed_bar.addClass('is-slideout');
 		}
-    });
+		});
 
 	
 	/**
@@ -251,9 +252,9 @@
 	 * @param  {[type]} option [プラグイン呼び出し時のオプション]
 	 */
 	$.fn.simpleDrawer = function(option) {
-        return this.each(function() {
-            new $.simpleDrawer(this, option);
-        });
-    };
+				return this.each(function() {
+						new $.simpleDrawer(this, option);
+				});
+		};
 
 })(jQuery);
